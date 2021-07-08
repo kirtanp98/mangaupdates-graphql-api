bd6fbd2ca918b98e089fb0a72a5fbe3d079ff8df
Kirtan Patel <kirtanpatl@gmail.com>
2021-07-07 20:54:16 -0400

    Add guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { join } from 'path';
+import { GqlThrottlerGuard } from './guard/GraphQLGuard';
 import { MangaModule } from './manga/manga.module';
 import { ScrapperModule } from './scrapper/scrapper.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 60,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       introspection: true,
@@ -15,6 +22,11 @@ import { ScrapperModule } from './scrapper/scrapper.module';
     ScrapperModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: GqlThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/guard/GraphQLGuard.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ThrottlerGuard } from '@nestjs/throttler';
+
+@Injectable()
+export class GqlThrottlerGuard extends ThrottlerGuard {
+  getRequestResponse(context: ExecutionContext) {
+    const gqlCtx = GqlExecutionContext.create(context);
+    const ctx = gqlCtx.getContext();
+    console.log(ctx.req);
+    return { req: ctx.req, res: ctx.req.res };
+  }
+}
