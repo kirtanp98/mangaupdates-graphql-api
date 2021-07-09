ffdfe17af5fc41f520e2237675ca977aa09fd54f
Kirtan Patel <kirtanpatl@gmail.com>
2021-07-08 20:11:59 -0400

    Test deployment

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('Starting api:');
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
