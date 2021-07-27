e215a7e3b75864e06eef5518a79ee3e6bab9470d
Kirtan Patel <kirtanpatl@gmail.com>
2021-07-26 21:14:12 -0400

    Fix search param builder

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -6,6 +6,7 @@ import {
   SeriesSearchItem,
 } from './entities/search.entity';
 import { ItemsPerPage, ResultType } from './entities/search.enum';
+import fetch from 'node-fetch';
 
 @Injectable()
 export class SearchService {
@@ -38,22 +39,24 @@ export class SearchService {
   async seriesSearch(
     searchInput: SearchInput,
   ): Promise<[SeriesSearchItem[], number]> {
-    const params = this.seriesSearchParamBuilder(searchInput);
-
-    const data = await fetch('https://www.mangaupdates.com/search.html', {
-      method: 'POST',
-      body: params,
+    const url = this.seriesSearchURLBuilder(searchInput);
+    const data = await fetch(url, {
+      method: 'GET',
     });
     const html = await data.text();
 
     return [[], 1];
   }
 
-  private seriesSearchParamBuilder(searchInput: SearchInput): URLSearchParams {
+  private seriesSearchURLBuilder(searchInput: SearchInput): string {
+    const url = 'https://www.mangaupdates.com/series.html?';
     const searchParams = new URLSearchParams();
-    searchParams.append('search', searchInput.search);
-    searchParams.append('orderby', searchInput.sortModel.field);
 
-    return searchParams;
+    searchParams.append('search', searchInput.search);
+    if (searchInput.sortModel) {
+      searchParams.append('orderby', searchInput.sortModel.field);
+    }
+
+    return url + searchParams;
   }
 }
